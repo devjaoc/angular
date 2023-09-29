cd69919bcf6bfb1ad3b4a3aea98adaaa6ad0f615
Jose Antonio Ortiz Corona <dwlince@gmail.com>
2023-09-29 15:05:10 -0600

    Introduccion a angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mi-proyecto';
+  title = 'Valor 1';
+  textoPlaceholder = "Escribe algo aqui";
+  imgSrc = "";
+  texto="";
+  constructor() {
+    setInterval(() => this.title = "Nuevo valor", 3000);
+  }
+  getSuma(numero1: number, numero2: number) {
+    return numero1 + numero2;
+  }
+  cambiarTitulo() {
+    this.title = this.texto;
+  }
 }
