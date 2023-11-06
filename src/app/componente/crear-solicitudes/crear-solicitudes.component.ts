cc54b9814f05f47fa369231d3bd6212e6de4082d
Jose Antonio Ortiz Corona <dwlince@gmail.com>
2023-11-06 13:39:07 -0600

    Tarea agregar metodo de editar solicitud

--- src/app/componente/crear-solicitudes/crear-solicitudes.component.ts
+++ src/app/componente/crear-solicitudes/crear-solicitudes.component.ts
@@ -11,11 +11,31 @@ export class CrearSolicitudesComponent {
   solicitud!: Solicitud;
   mensaje!: string;
   constructor(private solicitudService: SolicitudService) {
+    //tener el id parametro
+    //en caso que sea null
     this.solicitud = new Solicitud(-1, '', '', 0);
+    //no sea null 
+    //realizar solicitude de servidor 
+    /* 
+    solicitudService.get(id).
+    .subscribe({
+      next: (resp) => {
+        console.log(resp);
+        this.solicitud =resp.data---> Solicitud
+         this.solicitud = new Solicitud(resp.data.id, resp.data.titulo_corto, resp.data.descripcion, 0);
+      },  
+      error: (err) => {
+        console.log(err.error.msg);
+      },
+    });
+    */
+
   }
   submit() {
     //servidor.crearSolicitud(this.solicitud);
     this.solicitud.estado_solicitud_id = 1;
+    //modificar para que si tiene definido id elemento solicitud se actualice si no 
+    //se crea nuevo 
     this.solicitudService.create(this.solicitud).subscribe({
       next: (resp) => {
         console.log(resp);
--- src/config/env.ts
+++ src/config/env.ts
@@ -1,30 +1,30 @@
-import { Routes } from "@angular/router";
-import { CrearSolicitudesComponent } from "src/app/componente/crear-solicitudes/crear-solicitudes.component";
-import { HomeComponent } from "src/app/componente/home/home.component";
-import { ListaSolicitudesComponent } from "src/app/componente/lista-solicitudes/lista-solicitudes.component";
-import { SolicitudesComponent } from "src/app/componente/solicitudes/solicitudes.component";
+import { Routes } from '@angular/router';
+import { CrearSolicitudesComponent } from 'src/app/componente/crear-solicitudes/crear-solicitudes.component';
+import { HomeComponent } from 'src/app/componente/home/home.component';
+import { ListaSolicitudesComponent } from 'src/app/componente/lista-solicitudes/lista-solicitudes.component';
+import { SolicitudesComponent } from 'src/app/componente/solicitudes/solicitudes.component';
 
 export class ItemMenu {
-    public id!: number;
-    public name!: string;
-    public href!: string;
-    constructor(id: number, name: string, href: string) {
-        this.id = id;
-        this.name = name;
-        this.href = href;
-    }
+  public id!: number;
+  public name!: string;
+  public href!: string;
+  constructor(id: number, name: string, href: string) {
+    this.id = id;
+    this.name = name;
+    this.href = href;
+  }
 }
 export namespace env {
-    export const menu: Array<ItemMenu> = [
-        new ItemMenu(1, 'Inicio', '/'),
-        new ItemMenu(2, 'Lista Solicitudes', 'lista-solicitudes'),
-        new ItemMenu(3, 'Crear Solicitudes', 'crear-solicitudes'),
-    ];
-    export const routers: Routes = [
-        { path: '', component: HomeComponent },
-        { path: 'lista-solicitudes', component: ListaSolicitudesComponent },
-        { path: 'crear-solicitudes', component: CrearSolicitudesComponent },
-        { path: 'ver-solicitudes', component: SolicitudesComponent },
-    ];
-
+  export const menu: Array<ItemMenu> = [
+    new ItemMenu(1, 'Inicio', '/'),
+    new ItemMenu(2, 'Lista Solicitudes', 'lista-solicitudes'),
+    new ItemMenu(3, 'Crear Solicitudes', 'crear-solicitudes'),
+  ];
+  export const routers: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'lista-solicitudes', component: ListaSolicitudesComponent },
+    { path: 'crear-solicitudes', component: CrearSolicitudesComponent },
+    { path: 'editar-solicitud/:id', component: CrearSolicitudesComponent },
+    { path: 'ver-solicitudes', component: SolicitudesComponent },
+  ];
 }
